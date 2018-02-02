bbfac98ed2f8dfd6eb06beab39e1ef31daf0299a
vsjakhar <vijayjakhar.vj@gmail.com>
2018-02-02 18:18:32 +0530

    Default Template Page settings

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from .models import Scrap
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def scrap(request):
+    a_list = Scrap.objects.all()
+    context = {'year': '2018', 'article_list': a_list}
+    return render(request, 'demo/scrap.html', context)
