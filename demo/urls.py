fa50240e74a8f5d35f08acbfb0eba8edbeb0058f
vsjakhar <vijayjakhar.vj@gmail.com>
2018-02-03 20:04:24 +0530

    Data Scrapping from url in many different way using BS4

--- demo/urls.py
+++ demo/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('scrap', views.scrap, name='scrap'),
+    path('scrapa', views.scrapa, name='scrapa'),
+    path('scrapta', views.scrapta, name='scrapta'),
+    path('scrapcls', views.scrapcls, name='scrapcls'),
 ]
--- demo/views.py
+++ demo/views.py
@@ -5,9 +5,49 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Scrap
 
+import json
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	html = urlopen("http://www.donbest.com/nfl/odds/20180204.html")
+	# print(html)
+	soup = BeautifulSoup(html, 'html.parser')
+	finalh = soup.prettify()
+	#print(soup.prettify())
+	return HttpResponse(finalh)
+
+def scrapa(request):
+	html = urlopen("http://www.donbest.com/nfl/odds/20180204.html")
+	soup = BeautifulSoup(html, 'html.parser')
+	links = []
+	for link in soup.find_all('a'):
+		links.append([link.get('href')])
+		print(link.get('href'))
+	finalh = json.dumps({'data':links})
+	return HttpResponse(finalh, content_type='application/json')
+
+def scrapta(request):
+	html = urlopen("http://www.donbest.com/nfl/odds/20180204.html")
+	soup = BeautifulSoup(html, 'html.parser')
+	links = []
+	for link in soup.find('table').find_all('a'):
+		links.append([link.get('href')])
+		print(link.get('href'))
+	finalh = json.dumps({'data':links})
+	return HttpResponse(finalh, content_type='application/json')
+
+def scrapcls(request):
+	html = urlopen("http://www.donbest.com/nfl/odds/20180204.html")
+	soup = BeautifulSoup(html, 'html.parser')
+	links = []
+	for link in soup.find('div',class_='odds_gamesHolder').find_all('a'):
+		links.append([link.get('href')])
+		print(link.get('href'))
+	finalh = json.dumps({'data':links})
+	return HttpResponse(finalh, content_type='application/json')
 
 def scrap(request):
     a_list = Scrap.objects.all()
